71ec46f2f51ee3666ed80d193eee408af4ebc78b
ravchappa22 <chapparavi22@gmail.com>
2017-08-16 09:24:30 -0400

    First Commit

---
+++ src/test/java/com/my/company/jenkins/app/MyJenkinsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.my.company.jenkins.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyJenkinsDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
